8b50c8905e07b2bcfa08f1a58a8b688026420ee3
Simon-Bolivar99 <gartem99@yandex.ru>
2023-06-03 22:53:30 +0300

    Добавление файлов для парсинга

---
+++ lib_src/TerminalOptions.hpp
@@ -0,0 +1,2 @@
+#include <string>
+#include <boost/program_options.hpp>
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "lib_src/sgp4sdp4.hpp"
+#include "lib_src/TerminalOptions.hpp"
+int main(int, char**) 
+{
+    std::cout<< isFlagSet(1)<<std::endl;
+  return 0;  
+}
