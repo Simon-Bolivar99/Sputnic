66c697871f8b856178b4f92ab4830717149e254e
Simon-Bolivar99 <gartem99@yandex.ru>
2023-06-07 21:51:55 +0300

    сборка

---
+++ build/CMakeFiles/sgp4sdp4.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/artyom/Документы/C++ PROJECT/SPUTNIC/lib_src/sgp4sdp4.cpp" "CMakeFiles/sgp4sdp4.dir/lib_src/sgp4sdp4.cpp.o" "gcc" "CMakeFiles/sgp4sdp4.dir/lib_src/sgp4sdp4.cpp.o.d"
+  "/home/artyom/Документы/C++ PROJECT/SPUTNIC/lib_src/sgp_math.cpp" "CMakeFiles/sgp4sdp4.dir/lib_src/sgp_math.cpp.o" "gcc" "CMakeFiles/sgp4sdp4.dir/lib_src/sgp_math.cpp.o.d"
+  "/home/artyom/Документы/C++ PROJECT/SPUTNIC/lib_src/sgp_time.cpp" "CMakeFiles/sgp4sdp4.dir/lib_src/sgp_time.cpp.o" "gcc" "CMakeFiles/sgp4sdp4.dir/lib_src/sgp_time.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/sgp4sdp4.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sgp4sdp4.dir/lib_src/sgp4sdp4.cpp.o"
+  "CMakeFiles/sgp4sdp4.dir/lib_src/sgp4sdp4.cpp.o.d"
+  "CMakeFiles/sgp4sdp4.dir/lib_src/sgp_math.cpp.o"
+  "CMakeFiles/sgp4sdp4.dir/lib_src/sgp_math.cpp.o.d"
+  "CMakeFiles/sgp4sdp4.dir/lib_src/sgp_time.cpp.o"
+  "CMakeFiles/sgp4sdp4.dir/lib_src/sgp_time.cpp.o.d"
+  "libsgp4sdp4.a"
+  "libsgp4sdp4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sgp4sdp4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
