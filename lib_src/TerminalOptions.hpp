805aa2c090622bad8e564e8b0dee5bd7c93f8ada
Simon-Bolivar99 <gartem99@yandex.ru>
2023-06-07 17:13:01 +0300

    парсер

--- lib_src/TerminalOptions.hpp
+++ lib_src/TerminalOptions.hpp
@@ -1,2 +1,16 @@
+#ifndef _TERMINAL_OPTIONS_HPP_
+#define _TERMINAL_OPTIONS_HPP_
+
 #include <string>
 #include <boost/program_options.hpp>
+namespace po = boost::program_options;
+
+/*
+[-h| --help]                        Help menu
+[-f| --file] ARG (std::string)      Input file
+[-s| --start] ARG (double)          Start time
+[-e| --end ] ARG (double)           End time
+*/
+
+
+#endif //_TERMINAL_OPTIONS_HPP_
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-#include "lib_src/sgp4sdp4.hpp"
 #include "lib_src/TerminalOptions.hpp"
+#include "lib_src/sgp4sdp4.hpp"
+
 int main(int, char**) 
 {
     std::cout<< isFlagSet(1)<<std::endl;
