2d9ccb5eb7c62c954292c11782997c1e51795b99
Simon-Bolivar99 <gartem99@yandex.ru>
2023-06-19 21:18:47 +0300

    Создание класса

--- lib_src/TerminalOptions.hpp
+++ lib_src/TerminalOptions.hpp
@@ -6,11 +6,52 @@
 namespace po = boost::program_options;
 
 /*
-[-h| --help]                        Help menu
-[-f| --file] ARG (std::string)      Input file
-[-s| --start] ARG (double)          Start time
-[-e| --end ] ARG (double)           End time
+[-h| --help ]                        Help menu
+[-f| --file ] ARG (std::string)      Input file
+[-s| --start] ARG (double)           Start time
+[-e| --end  ] ARG (double)           End time
+[-c| --crd  ] ARG (str)              latitude longitude height
 */
 
+class TerminalOptions {
+    public:
+    enum statusReturn{
+        OPTS_SUCESS,
+        OPTS_VERSION,
+        OPTS_HELP,
+        OPTS_FAILURE
+
+    };
+
+    TerminalOptions();
+    ~TerminalOptions();
+    statusReturn parse(int argc, char * argv[]);
+
+    inline const std::string & getInputFile() const;
+    inline const double getStartTime() {return startTime;};
+    inline const double getEndTime() {return endTime;};
+    inline const std::string & getCoord() const;
+
+    protected:
+    void setup();
+    bool validateFiles();
+
+    private:
+    TerminalOptions(const TerminalOptions &rhs);
+    TerminalOptions &operator=(const TerminalOptions &rhs);
+
+    po::options_description myOptions;
+    std::string myInputFile;
+    double startTime;
+    double endTime;
+
+
+
+
+};
+inline const std::string & TerminalOptions::getInputFile() const{
+    static const std::string emptyString;
+    return (0 < myInputFile.size())? myInputFile : emptyString;
+}
 
 #endif //_TERMINAL_OPTIONS_HPP_
